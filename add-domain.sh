f22f56717fee8ed61308fb2892b3a26f8c4945c7
Diego Plencovich <diego@plencovich.com>
2016-01-18 17:53:07 -0300

    Fix para crear los archivos .conf automaticamente

--- add-domain.sh
+++ add-domain.sh
@@ -5,16 +5,35 @@ echo "Ingrese el usuario: "
 read Usuario
 sudo adduser $Usuario
 sudo mkdir -p /var/www/$Dominio/public_html
-sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$Dominio.conf
-sudo nano /etc/apache2/sites-available/$Dominio.conf
+echo "<VirtualHost *:80>" > $Dominio.conf
+echo "	ServerName $Dominio" >> $Dominio.conf
+echo "	ServerAlias www.$Dominio" >> $Dominio.conf
+echo "	DocumentRoot /var/www/$Dominio/public_html" >> $Dominio.conf
+echo "" >> $Dominio.conf
+echo "	<Directory />" >> $Dominio.conf
+echo "		Options FollowSymLinks" >> $Dominio.conf
+echo "		AllowOverride None" >> $Dominio.conf
+echo "	</Directory>" >> $Dominio.conf
+echo "	<Directory /var/www/$Dominio/public_html>" >> $Dominio.conf
+echo "		Options -Indexes" >> $Dominio.conf
+echo "		AllowOverride All" >> $Dominio.conf
+echo "		Order allow,deny" >> $Dominio.conf
+echo "		allow from all" >> $Dominio.conf
+echo "	</Directory>" >> $Dominio.conf
+echo "" >> $Dominio.conf
+echo "	ErrorLog /var/www/$Dominio/error.log" >> $Dominio.conf
+echo "	CustomLog /var/www/$Dominio/access.log combined" >> $Dominio.conf
+echo "" >> $Dominio.conf
+echo "</VirtualHost>" >> $Dominio.conf
+sudo mv $Dominio.conf /etc/apache2/sites-available/
 sudo a2ensite $Dominio.conf
 sudo cp /var/www/html/newdomain.php /var/www/$Dominio/public_html/index.php
 sudo cp /var/www/html/dominio.jpg /var/www/$Dominio/public_html/dominio.jpg
-sudo ln -s /var/www/$Dominio/public_html/ /home/$Usuario/html
+sudo ln -s /var/www/$Dominio/pubic_html/ /home/$Usuario/html
 sudo chown -R $Usuario:$Usuario /var/www/$Dominio/public_html
 sudo chmod -R 755 /var/www/$Dominio/
 clear
-sudo service apache2 restart
+service apache2 restart
 echo 
 echo 
 echo "El Dominio: '$Dominio' se ha creado Ok con el usuario: '$Usuario'." 
--- park-domain.sh
+++ park-domain.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 echo "Ingrese el dominio a parkear: "
 read DominioPark
-sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$DominioPark.conf
-sudo nano /etc/apache2/sites-available/$DominioPark.conf
-sudo a2ensite $DominioPark.conf
+echo "<VirtualHost *:80>" > $DominioPark.conf
+echo "	ServerName $DominioPark" >> $DominioPark.conf
+echo "	ServerAlias www.$DominioPark" >> $DominioPark.conf
+echo "	DocumentRoot /var/www/$DominioPark/public_html" >> $DominioPark.conf
+echo "" >> $DominioPark.conf
+echo "	<Directory />" >> $DominioPark.conf
+echo "		Options FollowSymLinks" >> $DominioPark.conf
+echo "		AllowOverride None" >> $DominioPark.conf
+echo "	</Directory>" >> $DominioPark.conf
+echo "	<Directory /var/www/$DominioPark/public_html>" >> $DominioPark.conf
+echo "		Options -Indexes" >> $DominioPark.conf
+echo "		AllowOverride All" >> $DominioPark.conf
+echo "		Order allow,deny" >> $DominioPark.conf
+echo "		allow from all" >> $DominioPark.conf
+echo "	</Directory>" >> $DominioPark.conf
+echo "" >> $DominioPark.conf
+echo "	ErrorLog /var/www/$DominioPark/error.log" >> $DominioPark.conf
+echo "	CustomLog /var/www/$DominioPark/access.log combined" >> $DominioPark.conf
+echo "" >> $DominioPark.conf
+echo "</VirtualHost>" >> $DominioPark.conf
+sudo mv $DominioPark.conf /etc/apache2/sites-available/
+sudo a2ensite $DominioParkPark.conf
 clear
 sudo service apache2 restart
 echo 
 echo 
-echo "El Dominio: '$DominioPark' se ha agregado Ok"
+echo "El Dominio: '$DominioParkPark' se ha agregado Ok"

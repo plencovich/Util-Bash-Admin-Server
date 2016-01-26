9042ae630924f5748ee6050c6733e08ba7fd6dd4
Diego Plencovich <diego@plencovich.com>
2016-01-26 09:19:38 -0300

    fix que apunta al dominio padre

--- park-domain.sh
+++ park-domain.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 echo "Ingrese el dominio a parkear: "
 read DominioPark
+echo "Ingrese el dominio Padre: "
+read DominioOrigianl
 echo "<VirtualHost *:80>" > $DominioPark.conf
 echo "	ServerName $DominioPark" >> $DominioPark.conf
 echo "	ServerAlias www.$DominioPark" >> $DominioPark.conf
-echo "	DocumentRoot /var/www/$DominioPark/public_html" >> $DominioPark.conf
+echo "	DocumentRoot /var/www/$DominioOrigianl/public_html" >> $DominioPark.conf
 echo "" >> $DominioPark.conf
 echo "	<Directory />" >> $DominioPark.conf
 echo "		Options FollowSymLinks" >> $DominioPark.conf
 echo "		AllowOverride None" >> $DominioPark.conf
 echo "	</Directory>" >> $DominioPark.conf
-echo "	<Directory /var/www/$DominioPark/public_html>" >> $DominioPark.conf
+echo "	<Directory /var/www/$DominioOrigianl/public_html>" >> $DominioPark.conf
 echo "		Options -Indexes" >> $DominioPark.conf
 echo "		AllowOverride All" >> $DominioPark.conf
 echo "		Order allow,deny" >> $DominioPark.conf
 echo "		allow from all" >> $DominioPark.conf
 echo "	</Directory>" >> $DominioPark.conf
 echo "" >> $DominioPark.conf
-echo "	ErrorLog /var/www/$DominioPark/error.log" >> $DominioPark.conf
-echo "	CustomLog /var/www/$DominioPark/access.log combined" >> $DominioPark.conf
+echo "	ErrorLog /var/www/$DominioOrigianl/error.log" >> $DominioPark.conf
+echo "	CustomLog /var/www/$DominioOrigianl/access.log combined" >> $DominioPark.conf
 echo "" >> $DominioPark.conf
 echo "</VirtualHost>" >> $DominioPark.conf
 sudo mv $DominioPark.conf /etc/apache2/sites-available/
-sudo a2ensite $DominioParkPark.conf
+sudo a2ensite $DominioPark.conf
 clear
 sudo service apache2 restart
 echo 
 echo 
-echo "El Dominio: '$DominioParkPark' se ha agregado Ok"
+echo "El Dominio: '$DominioPark' se ha agregado y apudta a '$DominioOrigianl' Ok"
